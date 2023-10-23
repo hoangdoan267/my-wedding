1d3591577603e156f6299c517e01db8c96f95998
Hoang Doan <hoang.doan@galaxyfinx.com>
2023-10-23 10:18:17 +0700

    Remove Read more button

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(8080, () => {
+app.listen(9000, () => {
   console.log("Server successfully running on port 8080");
 });
